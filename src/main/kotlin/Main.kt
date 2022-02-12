a5678b12f7b22ca09006d1c3266bfe5bf1174c67
mercyndungukirigo@gmail.com <mercyndungukirigo>
2022-02-12 10:21:29 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(){
+
+    var name:String = "Mercy Kirigo Ndungu"
+    println(name)
+    var age = 23
+    println(age)
+    var phonenumber:String = "0768277620"
+    println(phonenumber)
+    var weight:Double = 56.67
+    println(weight)
+    var nationalitykenyan:Boolean = true
+    println(nationalitykenyan)
+}
+
+
